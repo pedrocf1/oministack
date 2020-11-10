742cc5e2231939a6465fea8df3057d9fe919c531
pcf <pcf@cesar.org.br>
2020-11-10 07:42:15 -0300

    creating base structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-
-
-app.get('/',(req, res)=>{
-    res.json({irra:"Irrá"});
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
 
---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/users',(req, res)=>{
+    res.json({irra:"Irrá"});
+})
+
+
+module.exports = routes;

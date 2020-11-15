bb802c7b68af690ef3659bfe8e79df51b9c9e080
Pedro Freire <pedrofc31.31@gmail.com>
2020-11-15 15:18:50 -0300

    implementing session, controllers and adding cors

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,13 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(req,res){
+        const id = req.headers.authorization;
+        
+        const incidents = await connection('incidents').where('ong_id',id).select('*');
+
+        return res = res.json(incidents);
+    }
+
+}
